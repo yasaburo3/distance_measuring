c0d67fd241cf2dc0789a38ea125808d4a350aaa5
yasaburo3 <42105276+yasaburo3@users.noreply.github.com>
2022-09-10 14:12:54 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yasaburo3/project/rs_ws/src
-# Build directory: /home/yasaburo3/project/rs_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("realsense-ros/realsense2_description")
-subdirs("realsense-ros/realsense2_camera")
